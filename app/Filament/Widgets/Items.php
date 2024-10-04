4ebf2f283710ff13c23965d91164f8836f04fe53
Ryan S <ryansyachdp@gmail.com>
2024-10-04 07:32:02 +0700

    feat: add dashboard

---
+++ app/Filament/Widgets/Items.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Filament\Resources\ItemResource;
+use Illuminate\Support\Facades\Auth;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class Items extends BaseWidget
+{
+    protected static ?int $sort = 3;
+
+    protected static ?string $heading = 'Stok Barang';
+
+    protected int | string | array $columnSpan = 'full';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                ItemResource::getEloquentQuery()
+            )
+            ->defaultPaginationPageOption(5)
+            ->defaultSort('quantity', 'asc')
+            ->columns([
+                Tables\Columns\TextColumn::make('No')
+                    ->rowIndex()
+                    ->alignCenter()
+                    ->width('60px'),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nama Barang')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('category.name')
+                    ->label('Kategori')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->label('Jumlah')
+                    ->numeric()
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('Gudang')
+                    ->searchable()
+                    ->sortable()
+                    ->visible(Auth::user()->role === 'super-admin'),
+            ]);
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,13 +37,13 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
-            // ->pages([
-            //     Pages\Dashboard::class,
-            // ])
+            ->pages([
+                Pages\Dashboard::class,
+            ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\AccountWidget::class,
+                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -68,7 +68,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->brandLogo(asset('images/bpsdm.png'))
             ->brandLogoHeight('2.5rem')
-            ->spa();
+            ->spa()
+            ->sidebarWidth('300px');
         // ->databaseNotifications();
     }
 }
